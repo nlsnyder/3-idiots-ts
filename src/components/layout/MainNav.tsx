f86a87cc1e5767d4eacd7eb44054b077c63c3949
Nathan Snyder <snides111@gmail.com>
2022-07-17 21:36:57 -0500

    use dummy svg logo, make navbar fully responsive, implement login buttons, just need to do hamburger menu

--- src/components/layout/MainNav.tsx
+++ src/components/layout/MainNav.tsx
@@ -1,27 +1,25 @@
 import React from "react";
-import styles from './MainNav.module.css';
+import styles from "./MainNav.module.css";
+import logo from "../../img/logoipsum-logo-27.svg";
 
 const MainNav: React.FC<{}> = (props) => {
   return (
-    <div className={`container my-3 ${styles.containerStyle}`}>
+    <div className={`container-fluid container-lg my-3 ${styles.containerStyle}`}>
       <div className="row">
-        <div className="col-md-12 col-lg-10 m-auto">
+        <div className="col-12 col-xl-10 m-auto">
           <div className="d-flex justify-content-between align-items-md-center">
-            <div>Testing</div>
-            <div>
-              <div className={styles.list}>
-                <a href="/">
-                  Listen Now
-                </a>
-                <a href="/">
-                  The Hosts
-                </a>
-                <a href="/contact-us">
-                  Contact Us
-                </a>
-              </div>
+            <a href="/">
+              <img src={logo} alt="3 idiots logo" />
+            </a>
+            <div className={styles.list}>
+              <a href="/">Listen Now</a>
+              <a href="/">The Hosts</a>
+              <a href="/contact-us">Contact Us</a>
             </div>
-            <div>Log In</div>
+            <div className={styles.list}>
+              <button type="button" className={styles.secondaryButton}>Log In</button>
+              <button type="button" className={styles.primaryButton}>Sign Up</button>
+            </div>
           </div>
         </div>
       </div>
