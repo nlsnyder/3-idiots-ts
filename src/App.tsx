e783ea74a13291a54d9e45bcd37d41cd9e83651a
Nathan Snyder <snides111@gmail.com>
2022-11-28 14:14:31 -0600

    navigate all unknown paths to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,7 @@ function App() {
             }
           />
           <Route path="/" element={<Navigate to={"/home"} replace />} />
+          <Route path="*" element={<Navigate to={"/home"} replace />} />
         </Routes>
         <Footer />
       </div>
